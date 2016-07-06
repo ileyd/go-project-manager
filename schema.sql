81ccc70e84b99758fba36a3cfdcb286c33b220ac
Eliot Whalan <ewhal@pantsu.cat>
2016-07-06 14:11:09 +1000

    Update schema

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `tests` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `company` longtext,
-  `email` longtext,
   `material` longtext,
+  `duedate` TIMESTAMP,
   `process` longtext,
   `samples` boolean,
   `testfile` boolean,
@@ -12,12 +12,17 @@ CREATE TABLE `tests` (
   `duedate` TIMESTAMP,
   `completion` longtext,
   `status` longtext,
+  `done` boolean,
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `users` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `email` longtext,
+  `company` longtext,
+  `contactname` longtext,
+  `phone` int(10),
+  `address` longtext,
   `password` longtext,
   `level` int(3),
   PRIMARY KEY (`id`)
