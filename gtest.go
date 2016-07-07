8e6f0b562610d75f5421bed7ead61691c500c64a
Eliot Whalan <ewhal@pantsu.cat>
2016-07-07 13:59:18 +1000

    fix up sql

--- gtest.go
+++ gtest.go
@@ -198,11 +198,11 @@ func newHandler(w http.ResponseWriter, r *http.Request) {
 		completion := r.FormValue("completion")
 		comments := r.FormValue("comments")
 
-		smt, err := db.Prepare("insert into tests(customer, datereceived, salesrep, samples, requirements, duedate, comments, done) values (?, ?, ?, ?, ?, ?, ?, ?, ?")
+		smt, err := db.Prepare("insert into tests(customer, datereceived, salesrep, samples, requirements, duedate, completion, comments, done) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?")
 		if err != nil {
 			log.Println(err)
 		}
-		_, err = smt.Exec(html.EscapeString(company), html.EscapeString(datereceived), html.EscapeString(salesrep), html.EscapeString(samples), html.EscapeString(requirements), html.EscapeString(duedate), html.EscapeString(comments), false)
+		_, err = smt.Exec(html.EscapeString(company), html.EscapeString(datereceived), html.EscapeString(salesrep), html.EscapeString(samples), html.EscapeString(requirements), html.EscapeString(duedate), html.EscapeString(completion), html.EscapeString(comments), false)
 		if err != nil {
 			log.Println(err.Error())
 		}
