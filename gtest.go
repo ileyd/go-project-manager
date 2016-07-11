617f11851331c148158d58002b9759e9424e75c7
Eliot Whalan <ewhal@pantsu.cat>
2016-07-11 17:04:16 +1000

    Finish up documentation

--- gtest.go
+++ gtest.go
@@ -291,85 +291,114 @@ func delHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if level == "admin" {
+		// get ID from url
 		vars := mux.Vars(r)
 		id := vars["id"]
+
+		// open db connection
 		db, err := sql.Open("mysql", DATABASE)
 		checkErr(err)
 
 		defer db.Close()
+
+		// delete database based on id
 		_, err = db.Query("delete from tests where id=?", html.EscapeString(id))
 		checkErr(err)
 
+		// Redirect back to homepage
 		http.Redirect(w, r, "/", 302)
 	}
 }
 func doneHandler(w http.ResponseWriter, r *http.Request) {
+	// get user level
 	_, level, err := getCookieVars(r)
 	if err != nil {
 		http.Redirect(w, r, "/login", 302)
 		return
 	}
 
+	// if user does not equal admin redirect to login page
 	if level != "admin" {
 		http.Redirect(w, r, "/login", 302)
 		return
 	}
 
 	if level == "admin" {
+		// get id based on url
 		vars := mux.Vars(r)
 		id := vars["id"]
+
+		// open db connection
 		db, err := sql.Open("mysql", DATABASE)
 		if err != nil {
 			log.Println(err)
 		}
-		defer db.Close()
-		var status bool
-		err = db.QueryRow("select done from tests where id=?", html.EscapeString(id)).Scan(&status)
 
+		defer db.Close()
+
+		// prepare status variable
+		var status bool
+
+		// query status from database into status var
+		err = db.QueryRow("select done from tests where id=?", html.EscapeString(id)).Scan(&status)
 		checkErr(err)
 
+		// if status is true set to false
 		if status == true {
 			_, err = db.Query("update tests set done=false where id=?", html.EscapeString(id))
 			checkErr(err)
 		}
+		// if status is false set to true
 		if status == false {
 			_, err = db.Query("update tests set done=true where id=?", html.EscapeString(id))
 			checkErr(err)
 		}
 
+		// Redirect back to home
 		http.Redirect(w, r, "/", 302)
 
 	}
 }
 
 func logoutHandler(w http.ResponseWriter, r *http.Request) {
+	// Prepare cookie struct with no variables
 	cookie := &http.Cookie{
 		Name:   "session",
 		Value:  "",
 		Path:   "/",
 		MaxAge: -1,
 	}
+
+	// set cookie
 	http.SetCookie(w, cookie)
+
+	// Redirect to login page
 	http.Redirect(w, r, "/login", 301)
 }
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
+		// load login template
 		err := templates.ExecuteTemplate(w, "login.html", "")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	case "POST":
-
+		// read email and password from form variables
 		email := r.FormValue("email")
 		password := r.FormValue("password")
+
+		// open db connection
 		db, err := sql.Open("mysql", DATABASE)
 		checkErr(err)
 
 		defer db.Close()
+
+		// declare variables for database results
 		var hashedPassword []byte
 		var level, name string
+		// read hashedPassword, name and level into variables
 		err = db.QueryRow("select password, name, level from users where email=?", html.EscapeString(email)).Scan(&hashedPassword, &name, &level)
 		if err == sql.ErrNoRows {
 			http.Redirect(w, r, "/login", 303)
@@ -377,29 +406,38 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		checkErr(err)
 
+		// compare bcrypt hash to userinput password
 		err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
 		if err == nil {
+			// prepare cookie
 			value := map[string]string{
 				"email": email,
 				"name":  name,
 				"level": level,
 			}
+			// encode variables into cookie
 			if encoded, err := cookieHandler.Encode("session", value); err == nil {
 				cookie := &http.Cookie{
 					Name:  "session",
 					Value: encoded,
 					Path:  "/",
 				}
+				// set user cookie
 				http.SetCookie(w, cookie)
 			}
+			// Redirect to home page
 			http.Redirect(w, r, "/", 302)
 		}
+		// Redirect to login page
 		http.Redirect(w, r, "/login", 302)
 
 	}
 
 }
+
+// newcompanyHandler handles making a new company
 func newcompanyHandler(w http.ResponseWriter, r *http.Request) {
+	// check if user is logged in
 	_, _, err := getCookieVars(r)
 	if err != nil {
 		http.Redirect(w, r, "/login", 302)
@@ -408,80 +446,117 @@ func newcompanyHandler(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 	case "GET":
+		// load company page
 		err := templates.ExecuteTemplate(w, "company.html", "")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	case "POST":
+		// read form input into variables
 		email := r.FormValue("email")
 		name := r.FormValue("name")
 		company := r.FormValue("company")
 		phone := r.FormValue("phone")
 		address := r.FormValue("address")
+
+		// open db connection
 		db, err := sql.Open("mysql", DATABASE)
 		checkErr(err)
 
 		defer db.Close()
+
+		// prepare db statement
 		smt, err := db.Prepare("insert into companies(email, company, contactname, phone, address) values(?, ?, ?, ?, ?)")
 		checkErr(err)
 
+		// escape and Execute db query
 		_, err = smt.Exec(html.EscapeString(email), html.EscapeString(company), html.EscapeString(name), html.EscapeString(phone), html.EscapeString(address))
 		checkErr(err)
 
-		http.Redirect(w, r, "/", 302)
+		// Redirect to newcompany
+		http.Redirect(w, r, "/newcompany", 302)
 
 	}
 
 }
 func registerHandler(w http.ResponseWriter, r *http.Request) {
+	// get user level
+	_, level, err := getCookieVars(r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+		return
+	}
+
+	// if user does not equal admin redirect to login page
+	if level != "admin" {
+		http.Redirect(w, r, "/login", 302)
+		return
+	}
+
 	switch r.Method {
 	case "GET":
+		// log register page
 		err := templates.ExecuteTemplate(w, "register.html", "")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	case "POST":
+		// read form input into variables
 		email := r.FormValue("email")
 		password := r.FormValue("password")
 		name := r.FormValue("name")
 		level := r.FormValue("level")
+
+		// open db connection
 		db, err := sql.Open("mysql", DATABASE)
 		checkErr(err)
 
 		defer db.Close()
+
+		// prepare insert query
 		smt, err := db.Prepare("insert into users(email, password, name, level) values(?, ?, ?, ?)")
 		checkErr(err)
 
+		// hash user password
 		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 		checkErr(err)
 
+		// escape and Execute query
 		_, err = smt.Exec(html.EscapeString(email), hashedPassword, html.EscapeString(name), html.EscapeString(level))
 		checkErr(err)
 
+		// Redirect to login page
 		http.Redirect(w, r, "/login", 302)
 
 	}
 
 }
 func filesHandler(w http.ResponseWriter, r *http.Request) {
+
+	// check if user is logged in
 	_, _, err := getCookieVars(r)
 	if err != nil {
 		http.Redirect(w, r, "/login", 302)
 		return
 	}
 
+	// get appnumber from url
 	vars := mux.Vars(r)
 	appnumber := vars["appnumber"]
+
+	// open db connection
 	db, err := sql.Open("mysql", DATABASE)
 	checkErr(err)
 
 	defer db.Close()
 	switch r.Method {
 	case "POST":
+		// read multipart form
 		reader, err := r.MultipartReader()
 		checkErr(err)
 
 		for {
+			// get the next part from multipart form
 			part, err := reader.NextPart()
 			if err == io.EOF {
 				break
@@ -491,36 +566,53 @@ func filesHandler(w http.ResponseWriter, r *http.Request) {
 				continue
 			}
 
+			// save filename to variable
 			file := part.FileName()
+
+			// create a new file in UPDIRECTORY and with filename
 			dst, err := os.Create(UPDIRECTORY + file)
 			checkErr(err)
 
 			defer dst.Close()
+
 			// save uploaded data to created file
 			_, err = io.Copy(dst, part)
 			checkErr(err)
 
+			// prepare insert query
 			smt, err := db.Prepare("insert into files(file, appnumber) values(?, ?)")
 			checkErr(err)
 
+			// Execute and escape db query
 			_, err = smt.Exec(file, html.EscapeString(appnumber))
 			checkErr(err)
 		}
 
+		// Redirect to files page
 		http.Redirect(w, r, "/files/"+appnumber, 302)
 
 	case "GET":
+		// prepare FilesPage struct
 		b := FilesPage{Files: []Files{}}
+		// declare app number to be appnumber from url
 		b.AppNumber = appnumber
+
+		// query database for all files with the same appnumber
 		rows, err := db.Query("select id, file, appnumber from files where appnumber=?", html.EscapeString(appnumber))
 		checkErr(err)
 
+		// scan db query data into Files struct
 		for rows.Next() {
+			// prepare Files struct
 			res := Files{}
+			// scan data into struct
 			rows.Scan(&res.ID, &res.Name, &res.AppNumber)
+			// append Files Struct to FilesPage struct
 			b.Files = append(b.Files, res)
 
 		}
+
+		// ExecuteTemplate files with FilesPage struct
 		err = templates.ExecuteTemplate(w, "files.html", &b)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -529,6 +621,7 @@ func filesHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 func putHandler(w http.ResponseWriter, r *http.Request) {
+	// get user level
 	_, level, err := getCookieVars(r)
 	if err != nil {
 		http.Redirect(w, r, "/login", 302)
@@ -540,14 +633,19 @@ func putHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// get id from url
 	vars := mux.Vars(r)
 	id := vars["id"]
+
+	// open db connection
 	db, err := sql.Open("mysql", DATABASE)
 	checkErr(err)
 
 	defer db.Close()
+
 	switch r.Method {
 	case "POST":
+		// read form values into variables
 		company := r.FormValue("company")
 		datereceived := r.FormValue("datereceived")
 		salesrep := r.FormValue("salesrep")
@@ -559,22 +657,31 @@ func putHandler(w http.ResponseWriter, r *http.Request) {
 		status := r.FormValue("status")
 		comments := r.FormValue("comments")
 
+		// Prepare update query
 		smt, err := db.Prepare("Update tests set customer=?, datereceived=?, salesrep=?, samples=?, requirements=?, duedate=?, dispatch=?, appnumber=?, status=?, comments=? where id=?")
 		checkErr(err)
 
+		// Execute and escape form values
 		_, err = smt.Exec(html.EscapeString(company), html.EscapeString(datereceived), html.EscapeString(salesrep), html.EscapeString(samples), html.EscapeString(requirements), html.EscapeString(duedate), html.EscapeString(dispatch), html.EscapeString(appnumber), html.EscapeString(status), html.EscapeString(comments), html.EscapeString(id))
 		checkErr(err)
 
+		// Redirect to home page
 		http.Redirect(w, r, "/", 302)
 
 	case "GET":
+		// prepare Tests struct
 		res := Tests{}
+
+		// query database for variables
 		rows, err := db.Query("select id, customer, datereceived, salesrep, samples, requirements, duedate, dispatch, appnumber, status, comments, done from tests where id=?", html.EscapeString(id))
 		checkErr(err)
 
+		// read variables from databse into Tests struct
 		for rows.Next() {
 			rows.Scan(&res.ID, &res.Customer, &res.DateReceived, &res.SalesRep, &res.Samples, &res.Requirements, &res.DueDate, &res.Dispatch, &res.AppNumber, &res.Status, &res.Comments, &res.Done)
 		}
+
+		// Execute modify template with tests struct
 		err = templates.ExecuteTemplate(w, "modify.html", &res)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -585,7 +692,9 @@ func putHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
+	// new mux router
 	router := mux.NewRouter()
+	// route handlers
 	router.HandleFunc("/new", newHandler)
 	router.HandleFunc("/newcompany", newcompanyHandler)
 	router.HandleFunc("/done/{id}", doneHandler)
@@ -597,7 +706,9 @@ func main() {
 	router.HandleFunc("/logout", logoutHandler)
 	router.HandleFunc("/register", registerHandler)
 	router.HandleFunc("/", ordersHandler)
+	// serve UPDIRECTORY
 	router.Handle("/static/{rest}", http.StripPrefix("/static/", http.FileServer(http.Dir(UPDIRECTORY))))
+	// ListenAndServe on PORT with router
 	err := http.ListenAndServe(PORT, router)
 	if err != nil {
 		log.Fatal(err)
